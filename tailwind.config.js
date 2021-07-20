7cfbf9f57f176fb9ccc28d6997b0855dfb16387e
paber26 <bernaldo.stis@gmail.com>
2021-07-21 00:12:07 +0700

    Pembuatan bagian Frame 4

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        coklatdecotatoo: '#C6A461'
+      },
+      fontFamily: {
+        questrial: ['Questrial'],
+        roboto: ['Roboto']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  }, 
+  plugins: [],
+}
